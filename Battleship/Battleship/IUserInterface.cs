c2ac23303edf33e4868a57ab7d8ac08e2beff591
Riasalit <daniel-vilslev@hotmail.com>
2019-04-02 08:26:55 +0200

    changed ui description

--- Battleship/Battleship/IUserInterface.cs
+++ Battleship/Battleship/IUserInterface.cs
@@ -4,14 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /***********************************************************************************
- * General interface for communicating with the user                               *
- * This interface needs a function for getting the user inputs: InteractWithUser() *
- * A function to return a complete ship: GetShip()                                 *
- * And a function to return 3 data Chars called: GetData()                         *
- * where:                                                                          *
- * Char[0] is the orientation 'H' or 'V'                                           *
- * Char[1] is the x coordinate of the ship's origin                                *
- * Char[2] is the y coordinate of the ship's origin                                *
+ * General interface for communicating with the user                          *
+ * This interface needs for a user to create the players: InitializePlayers() *
+ * A function to return a complete ship: GetShip()                            *
  * *********************************************************************************/
 namespace Battleship
 {
