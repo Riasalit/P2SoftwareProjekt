ef71d36f51860f486719d4ec2959c83a56c26232
Riasalit <daniel-vilslev@hotmail.com>
2019-04-02 08:28:02 +0200

    ehm.. fixede kanten på descriptionen til UI

--- Battleship/Battleship/IUserInterface.cs
+++ Battleship/Battleship/IUserInterface.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/***********************************************************************************
+/******************************************************************************
  * General interface for communicating with the user                          *
  * This interface needs for a user to create the players: InitializePlayers() *
  * A function to return a complete ship: GetShip()                            *
- * *********************************************************************************/
+ * ****************************************************************************/
 namespace Battleship
 {
     public interface IUserInterface
