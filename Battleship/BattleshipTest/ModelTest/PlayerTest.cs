9ab71ed942aef3b8f9686d7f412c235b9c0df789
Lena <durumguru@gmail.com>
2019-04-29 14:48:36 +0200

    Og lidt flere rettelser til test
    Meget små rettelser

--- Battleship/BattleshipTest/ModelTest/PlayerTest.cs
+++ Battleship/BattleshipTest/ModelTest/PlayerTest.cs
@@ -10,8 +10,6 @@ using System.IO;
 using BattleshipWeb;
 namespace BattleshipTest.ModelTest
 {
-    //Kan ikke få AI til at virke når jeg tester. Af en eller anden grund kan den ikke finde netcore.dll filerne
-    //Når vi engang løser det problem skal AI tilføjes til alle disse test's så de ikke længere kun omhandler humans
     [TestClass]
     public class PlayerTest
     {
@@ -29,6 +27,7 @@ namespace BattleshipTest.ModelTest
         public void CanHumanPlaceShipsCorrectly()
         {
             Player testHuman = new Human("TestHuman", new TestUI());
+
             testHuman.SetShips();
 
             Assert.AreEqual(Settings.shipCount, testHuman.board.ships.Count);
@@ -37,6 +36,7 @@ namespace BattleshipTest.ModelTest
         public void CanAIPlaceShipsCorrectly()
         {
             Player testAI = new Human("TestAI", new TestUI());
+
             testAI.SetShips();
 
             Assert.AreEqual(Settings.shipCount, testAI.board.ships.Count);
@@ -46,8 +46,9 @@ namespace BattleshipTest.ModelTest
         {
             TestUI testUI = new TestUI();
             Player testPlayer1 = new Human("TestPlayer1", testUI);
-            testPlayer1.SetShips();
             Player testPlayer2 = new Human("TestPlayer2", testUI);
+
+            testPlayer1.SetShips();
             testPlayer2.SetShips();
             testPlayer1.SetOpponent(testPlayer2);
             //Shoots untill all ships are sunken
@@ -62,11 +63,13 @@ namespace BattleshipTest.ModelTest
         {
             TestUI testUI = new TestUI();
             Player testHuman1 = new Human("TestHuman1", testUI);
-            testHuman1.SetShips();
             Player testHuman2 = new Human("TestHuman2", testUI);
+
+            testHuman1.SetShips();
             testHuman2.SetShips();
             testHuman1.SetOpponent(testHuman2);
             testHuman1.YourTurn();
+
             Assert.AreEqual(1, testHuman2.board.yourShots.Count);
         }
     }
