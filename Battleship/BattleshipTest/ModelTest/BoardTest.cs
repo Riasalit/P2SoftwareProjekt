9a0e66d8607e038c4780356e4727386dcaa578e4
Lenisa98 <36400245+Lenisa98@users.noreply.github.com>
2019-05-15 10:48:03 +0200

    Flere små-rettelser af unit tests. Perf x2 nu.
    EE

--- Battleship/BattleshipTest/ModelTest/BoardTest.cs
+++ Battleship/BattleshipTest/ModelTest/BoardTest.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Drawing;
-using HAPI;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Drawing;
 using BattleshipWeb;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 
 namespace BattleshipTest.ModelTest
 {
